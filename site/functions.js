0fa1373c88a651b3aa10aa795665b1892d9705dc
cassracp <cassracp@gmail.com>
2023-10-23 13:16:16 -0300

    Inclusão do Editor HTML

--- site/functions.js
+++ site/functions.js
@@ -126,7 +126,7 @@ function FormatarTopicoTarefa() {
     const numeroTarefa = document.getElementById("numeroTarefa").value;
     const tituloTopico = document.getElementById("tituloTopicoSelect").value;
     const osAssociada = document.getElementById("osAssociada").checked;
-    const numeroOS = document.getElementById("numeroOS").value;
+    const numeroOS = document.getElementById("numeroOS").value.trim();
 	const today = new Date();
 	const dd = String(today.getDate()).padStart(2, '0');
 	const mm = String(today.getMonth() + 1).padStart(2, '0'); // Janeiro é 0!
@@ -162,3 +162,88 @@ function FormatarTopicoTarefa() {
 	document.execCommand("copy");
 }
 /* FIM */
+
+
+
+/* Editor HTML*/
+function addTag(tag) {
+	const editor = document.getElementById("editor");
+	const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+	const newText = `<${tag}>${selectedText}</${tag}>`;
+	const start = editor.selectionStart;
+	const end = editor.selectionEnd;
+	editor.value = editor.value.substring(0, start) + newText + editor.value.substring(end);
+}
+
+function addBlockquote() {
+	const editor = document.getElementById("editor");
+	const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+	const newText = `<blockquote>${selectedText}</blockquote>`;
+	const start = editor.selectionStart;
+	const end = editor.selectionEnd;
+	editor.value = editor.value.substring(0, start) + newText + editor.value.substring(end);
+}
+
+function addPre() {
+	const editor = document.getElementById("editor");
+	const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+	const newText = `<pre>${selectedText}</pre>`;
+	const start = editor.selectionStart;
+	const end = editor.selectionEnd;
+	editor.value = editor.value.substring(0, start) + newText + editor.value.substring(end);
+}
+
+function addCode() {
+	const editor = document.getElementById("editor");
+	const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+	const newText = `<code>${selectedText}</code>`;
+	const start = editor.selectionStart;
+	const end = editor.selectionEnd;
+	editor.value = editor.value.substring(0, start) + newText + editor.value.substring(end);
+}
+
+function CopiarTexto(){
+	const textArea = document.getElementById("editor");
+	textArea.select();
+	document.execCommand("copy");
+}
+
+
+function LimparEditor() {
+	document.getElementById("editor").value = "";
+}
+
+//Spoiler
+function openModal() {
+	$('#spoilerModal').modal('show');
+}
+
+function UsarTituloModelo(){
+	const checkbox = document.getElementById('usarModelo');
+	const divSelecionarModelo = document.getElementById('divSelecionarModelo');
+	const divInserirTitulo = document.getElementById('divInserirTitulo');
+
+	if (checkbox.checked){
+		divSelecionarModelo.classList.remove("d-none");
+		divInserirTitulo.classList.add("d-none");
+	} else {
+		divSelecionarModelo.classList.add("d-none");
+		divInserirTitulo.classList.remove("d-none");
+	}
+}
+
+function insertSpoiler() {
+	const editor = document.getElementById("editor");
+	const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+	const checkbox = document.getElementById("usarModelo");
+	const tituloSpoiler = checkbox.checked ? document.getElementById("modeloTitulo").value : document.getElementById("tituloSpoiler").value;	
+	const start = editor.selectionStart;
+	const end = editor.selectionEnd;
+	const newText = `<div style="margin: 5px 20px 20px;"><div class="smallfont" style="margin-bottom: 2px;"><b>${tituloSpoiler}</b>: <input value="Open" style="margin: 0px; padding: 0px; width: 55px; font-size: 11px;" onclick="if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = ''; this.innerText = ''; this.value = 'Close'; } else { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none'; this.innerText = ''; this.value = 'Open'; }" type="button"></div><div class="alt2" style="border: 1px inset ; margin: 0px; padding: 6px;"><div style="display: none;">${selectedText}</div></div></div>`;
+
+	editor.value = editor.value.substring(0, start) + newText + editor.value.substring(end);
+	$('#spoilerModal').modal('hide');
+}
+//Fim spoiler
+
+/* FIM EDITOR HTML*/
