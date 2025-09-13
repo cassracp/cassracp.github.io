72ce520084d5352b6c41a539609eadf8d0a5049a
Cássio Rodolfo Alves de Carvalho Pinto <cassracp@gmail.com>
2025-09-13 19:03:33 -0300

    Adiciona script de build para instalar dependências do Chromium

---
+++ install-chromium-dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# install-chromium-dependencies.sh
+
+# Instala as dependências de sistema necessárias para o Chromium
+yum install -y alsa-lib \
+atk \
+at-spi2-atk \
+cups-libs \
+gtk3 \
+libdrm \
+libXcomposite \
+libXdamage \
+libXext \
+libXfixes \
+libXrandr \
+libXtst \
+mesa-libgbm \
+nss \
+pango \
+xorg-x11-server-Xvfb
